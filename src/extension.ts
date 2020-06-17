6f7fbd2e9e1611b8c7202123a5ee92ac6b40faa3
KurozeroPB <root@kurozeropb.info>
2020-06-17 18:06:40 +0200

    Update extension & dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -2,50 +2,45 @@ import * as vscode from "vscode";
 import SimpleGit from "simple-git/promise";
 import Yukikaze from "yukikaze";
 
-interface errorObj {
-    name?: string;
-    message?: string;
-}
-
 const git = SimpleGit(vscode.workspace.rootPath ?? process.cwd());
-
 const interval = new Yukikaze();
 
-// this method is called when your extension is activated
-export function activate(context: vscode.ExtensionContext) {
-	const config = vscode.workspace.getConfiguration();
-	const minutes = config.get<number>("commitReminder.interval") ?? 30;
-
-    git.checkIsRepo()
-        .then((isRepo: boolean) => isRepo && startInterval(minutes))
-        .catch((error: errorObj) => {
-            console.error('vscode-commit-reminder - no git repository found: ', error.name, ' ', error.message);
-        })
-
-	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((e) => {
-		if (e.affectsConfiguration("commitReminder.interval")) {
-			interval.stop();
-			const config = vscode.workspace.getConfiguration();
-			const minutes = config.get<number>("commitReminder.interval") ?? 30;
-			startInterval(minutes);
-		}
-	}));
+function startInterval(minutes: number): void {
+    interval.run(async () => {
+        try {
+            const result = await git.diffSummary();
+            if (result.changed > 0) {
+                vscode.window.showInformationMessage(`Don't forget to commit your new changes | ${result.changed} ${result.changed === 1 ? "file" : "files"} to commit`);
+            }
+        } catch (err) {
+            vscode.window.showErrorMessage(err ? err.toString() : "Error");
+        }
+    }, 1000 * 60 * minutes);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-	interval.stop();
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    const config = vscode.workspace.getConfiguration();
+    const minutes = config.get<number>("commitReminder.interval") ?? 30;
+
+    try {
+        const isRepo = await git.checkIsRepo();
+        if (isRepo) {
+            startInterval(minutes);
+        }
+    } catch (error) {
+        console.error(`vscode-commit-reminder - no git repository found: ${error.name} ${error.message}`);
+    }
+
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((e) => {
+        if (e.affectsConfiguration("commitReminder.interval")) {
+            interval.stop();
+            const config = vscode.workspace.getConfiguration();
+            const minutes = config.get<number>("commitReminder.interval") ?? 30;
+            startInterval(minutes);
+        }
+    }));
 }
 
-function startInterval(minutes: number) {
-	interval.run(async () => {
-		try {
-			const result = await git.diffSummary();
-			if (result.changed > 0) {
-				vscode.window.showInformationMessage(`Don't forget to commit your new changes | ${result.changed} ${result.changed === 1 ? "file" : "files"} to commit`);
-			}
-		} catch (err) {
-			vscode.window.showErrorMessage(err ? err.toString() : "Error");
-		}
-	}, 1000 * 60 * minutes);
+export function deactivate(): void {
+    interval.stop();
 }
